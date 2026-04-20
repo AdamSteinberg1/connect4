edf5440473c9b16f7e6706edabd4e62c0436a062
Adam <adam.l.steinberg@me.com>
2026-04-20 14:19:54 -0400

    broadcast server with channels

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,48 +1,58 @@
-use std::net::SocketAddr;
 use anyhow::Result;
-use std::sync::Arc;
+use std::net::SocketAddr;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::net::tcp::{OwnedWriteHalf};
-use tokio::sync::Mutex;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::TcpListener;
+use tokio::sync::broadcast::{Receiver, Sender};
+
+
+//TODO right now every message is broadcast to all connections
+// We want messages to only be broadcast to clients in the same "room"
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let writers: Arc<Mutex<Vec<OwnedWriteHalf>>> = Arc::new(Mutex::new(Vec::new()));
+
+    let (broadcast_tx, _) = tokio::sync::broadcast::channel::<String>(32);
 
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Listening on port 8080...");
 
     loop {
         let (stream, addr) = listener.accept().await?;
-        println!("New connection from {addr}");
+        let broadcast_tx = broadcast_tx.clone();
+        let broadcast_rx = broadcast_tx.subscribe();
 
-        let writers = writers.clone();
+        let (read_half, write_half) = stream.into_split();
+
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(stream, addr, writers).await {
-                println!("Error : {e}");
+            if let Err(e) = handle_reader(addr, broadcast_tx, read_half).await {
+                println!("Error while handling reader: {}", e);
+            }
+        });
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_writer(broadcast_rx, write_half).await {
+                println!("Error while handling writer: {}", e);
             }
         });
     }
 }
 
-async fn handle_connection(stream: TcpStream, addr: SocketAddr, writers: Arc<Mutex<Vec<OwnedWriteHalf>>>) -> Result<(), tokio::io::Error> {
-    let (reader, writer) = stream.into_split();
-
-    writers.lock().await.push(writer);
-
-    let reader = BufReader::new(reader);
-    let mut lines = reader.lines();
-
-    while let Some(line) = lines.next_line().await? {
-        let message = format!("[{addr}]: {line}\n");
-        print!("{}", message);
-        let mut writers = writers.lock().await;
-        for writer in writers.iter_mut() {
-            writer.write_all(message.as_bytes()).await?;
-        }
+async fn handle_writer(mut broadcast_rx: Receiver<String>, mut write_half: OwnedWriteHalf) -> Result<()> {
+    while let Ok(message) = broadcast_rx.recv().await {
+        write_half.write_all(message.as_bytes()).await?;
     }
+    Ok(())
 
-    println!("{addr} disconnected");
+}
+
+async fn handle_reader(addr: SocketAddr, broadcast_tx: Sender<String>, read_half: OwnedReadHalf) -> Result<()> {
+    let reader = BufReader::new(read_half);
+    let mut lines = reader.lines();
+    while let Some(line) = lines.next_line().await? {
+        let message = format!("{addr}: {line}\n");
+        print!("{message}");
+        broadcast_tx.send(message).map_err(|e| anyhow::anyhow!("Error sending message: {}", e))?;
+    }
     Ok(())
 }
